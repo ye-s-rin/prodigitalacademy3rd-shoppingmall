32609d876bc639fae50098bdeb4d9e061ffec613
ye-s-rin <pyr53540@gmail.com>
2024-05-08 09:07:51 +0900

    feat: ProductService.java

---
+++ src/main/java/com/example/shoppingmall/product/ProductService.java
@@ -0,0 +1,33 @@
+package com.example.shoppingmall.product;
+
+import java.util.Map;
+import java.util.regex.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Service;
+
+@Log
+@Service
+@AllArgsConstructor
+public class ProductService {
+
+    private ProductRepository productRepository;
+
+    public Product findProduct(int id) {
+        return productRepository.findProduct(id);
+    }
+
+    public Map findProducts() {
+        return this.productRepository.findProducts();
+    }
+
+    public Map pagination(int currentPage, int limit) {
+        return this.productRepository.pagination(currentPage, limit);
+    }
+
+    public void registerProduct(Product product) {
+        productRepository.registerProduct(product);
+        log.info("/product: service - " + product.getName());
+
+    }
+}
