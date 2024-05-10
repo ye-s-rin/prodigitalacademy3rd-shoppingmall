db38d6210e66ae2a84606b9304f1d303e2d87976
ye-s-rin <pyr53540@gmail.com>
2024-05-10 16:19:37 +0900

    refactor: add @ToString

--- src/main/java/com/example/shoppingmall/user/User.java
+++ src/main/java/com/example/shoppingmall/user/User.java
@@ -2,10 +2,12 @@ package com.example.shoppingmall.user;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
+@ToString
 @Component
 public class User {
 
