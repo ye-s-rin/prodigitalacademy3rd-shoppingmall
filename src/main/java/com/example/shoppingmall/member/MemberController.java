40bf380b8cb585b5c0e840c05ef75ee7e048e2d1
ye-s-rin <pyr53540@gmail.com>
2024-05-10 15:40:55 +0900

    feat: MemberController.java.java

---
+++ src/main/java/com/example/shoppingmall/member/MemberController.java
@@ -0,0 +1,13 @@
+package com.example.shoppingmall.member;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+public class MemberController {
+
+    private MemberController memberController;
+}
