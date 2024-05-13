7faee38986a51322462637b6a88155b68de69ad0
ye-s-rin <pyr53540@gmail.com>
2024-05-13 16:40:04 +0900

    fix: apply ApiResult

--- src/main/java/com/example/shoppingmall/user/UserController.java
+++ src/main/java/com/example/shoppingmall/user/UserController.java
@@ -18,34 +18,49 @@ public class UserController {
     private UserService userService;
 
     @PostMapping(value = "/join")
-    public ResponseEntity join(@RequestBody User user){
-        if(this.userService.isDuplicated(user.getUserId())){
-            return new ResponseEntity<>(HttpStatus.CONFLICT);
+    public ResponseEntity join(@RequestBody User user) {
+        /**
+         * ID 중복 체크
+         * 중복이면, 사용자 예외 클래스 소환
+         * 1) 예외 클래스한테 return 요청
+         * 2) 예외만 발생시키고 메시지는 직접 return
+         */
+        if (isDuplicateId(user)) {
+            DuplicateUserIdException e = new DuplicateUserIdException();
+            log.info(e.getMessage());
+
+//            return new ResponseEntity(null, HttpStatus.CONFLICT);
+            return new ResponseEntity(new ApiResult().join(null), HttpStatus.CONFLICT);
         }
 
         User signedUser = this.userService.join(user);
 
-        if(signedUser != null) {
+        if (signedUser != null) {
             Map<String, String> result = new HashMap<>();
             result.put("user_id", signedUser.getUserId());
-            return new ResponseEntity(result, HttpStatus.CREATED);
+//            return new ResponseEntity(result, HttpStatus.CREATED);
+            return new ResponseEntity(new ApiResult().join(signedUser), HttpStatus.CREATED);
         }
         return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
 
+    private boolean isDuplicateId(User user) {
+        return this.userService.isDuplicateId(user.getUserId());
+    }
+
     @PostMapping(value = "/duplication")
-    public ResponseEntity isDuplicated(@RequestBody Map<String, String> userId){
-        if(this.userService.isDuplicated(userId.get("user_id"))){
+    public ResponseEntity isDuplicateId(@RequestBody Map<String, String> userId) {
+        if (this.userService.isDuplicateId(userId.get("user_id"))) {
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @PostMapping(value = "/login")
-    public ResponseEntity login(@RequestBody Map<String, String> loginInfo){
+    public ResponseEntity login(@RequestBody Map<String, String> loginInfo) {
         User loginedUser = this.userService.login(loginInfo);
 
-        if(loginedUser != null){
+        if (loginedUser != null) {
             return new ResponseEntity(HttpStatus.OK);
         }
         return new ResponseEntity(HttpStatus.BAD_REQUEST);
