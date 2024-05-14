bf3f559697e91726e232732471e92dad14ff1b33
ye-s-rin <pyr53540@gmail.com>
2024-05-14 16:22:21 +0900

    feat: filter

---
+++ src/main/java/com/example/shoppingmall/order/FilterConfig.java
@@ -0,0 +1,17 @@
+package com.example.shoppingmall.order;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<MyFilter> myFilterRegistration() {
+        FilterRegistrationBean<MyFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new MyFilter());
+        registration.addUrlPatterns("/order/*"); // OrderController에만 적용
+        return registration;
+    }
+}
---
+++ src/main/java/com/example/shoppingmall/order/MyFilter.java
@@ -0,0 +1,39 @@
+package com.example.shoppingmall.order;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/*"})
+public class MyFilter implements Filter {
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		System.out.println("Filter - init");
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("Filter - destroy");
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        String requestURI = req.getRequestURI();
+
+        System.out.println("Filter Request - " + requestURI);
+        chain.doFilter(request, response);
+        System.out.println("Filter Response - " + requestURI);
+	}
+}
