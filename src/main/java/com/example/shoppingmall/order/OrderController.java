81ae5f8a7036a7c904f91869ae27293c9ff9c23c
ye-s-rin <pyr53540@gmail.com>
2024-05-14 16:22:39 +0900

    feat: OrderController.java

---
+++ src/main/java/com/example/shoppingmall/order/OrderController.java
@@ -0,0 +1,12 @@
+package com.example.shoppingmall.order;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+public class OrderController {
+
+}
