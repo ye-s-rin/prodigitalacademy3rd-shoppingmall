ce931c13899cd8b2c8f336b0b36172935b6e2b4c
ye-s-rin <pyr53540@gmail.com>
2024-05-20 13:19:28 +0900

    refactor: apply @Slf4j

--- src/main/java/com/example/shoppingmall/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/shoppingmall/exception/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
-package com.example.shoppingmall.utils;
+package com.example.shoppingmall.exception;
 
 import static com.example.shoppingmall.utils.ApiUtils.error;
 
+import com.example.shoppingmall.utils.ApiUtils.ApiResult;
 import java.util.HashMap;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,13 +13,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     // 유효성 검사에서 에러가 발생하면 호출되는 예외 처리 메소드
     @ExceptionHandler//(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiUtils.ApiResult<Map<String, String>> handleValidationExceptions(
+    public ApiResult<Map<String, String>> handleValidationExceptions(
         MethodArgumentNotValidException errors) {
         Map<String, String> errorMessages = new HashMap<>();
 
