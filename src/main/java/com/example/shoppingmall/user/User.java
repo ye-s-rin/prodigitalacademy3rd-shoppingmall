d2616b70bb4ab502b3d2e6076da48d1b3991b561
ye-s-rin <pyr53540@gmail.com>
2024-05-21 09:25:42 +0900

    refactor: apply @Entity

--- src/main/java/com/example/shoppingmall/user/User.java
+++ src/main/java/com/example/shoppingmall/user/User.java
@@ -1,6 +1,11 @@
 package com.example.shoppingmall.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -11,9 +16,12 @@ import org.springframework.stereotype.Component;
 @Setter
 @ToString
 @RequiredArgsConstructor
-@Component
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
     private int id;
     @JsonProperty(value = "user_id")
     private String userId;
@@ -22,7 +30,16 @@ public class User {
     private String email;
     private String contact;
 
+    public User(String userId, String pw, String name, String email, String contact) {
+        this.userId = userId;
+        this.pw = pw;
+        this.name = name;
+        this.email = email;
+        this.contact = contact;
+    }
+
     public User convertToDTO(UserDTO userDto) {
+        this.id = userDto.getId();
         this.userId = userDto.getUserId();
         this.pw = userDto.getPw();
         this.name = userDto.getName();
