2d30e856b1abca1a98637de2f3d49a62ab66476b
ye-s-rin <pyr53540@gmail.com>
2024-05-22 15:10:28 +0900

    feat: CustomApplicationRunner

--- src/main/java/com/example/shoppingmall/CustomApplicationRunner.java
+++ src/main/java/com/example/shoppingmall/CustomApplicationRunner.java
@@ -1,5 +1,9 @@
 package com.example.shoppingmall;
 
+import com.example.shoppingmall.user.User;
+import com.example.shoppingmall.user.UserJPARepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -7,8 +11,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class CustomApplicationRunner implements ApplicationRunner {
 
+    @Autowired
+    UserJPARepository userJPARepository;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("Application main과 동시 실행");
+
+        Optional<User> resultUser = this.userJPARepository.findByUserId("xxx");
+        System.out.println(resultUser.isEmpty() ? null : resultUser.get().getUserId());
     }
 }
