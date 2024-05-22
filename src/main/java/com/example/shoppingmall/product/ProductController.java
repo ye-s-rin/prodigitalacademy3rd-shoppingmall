6786768ae03303ea82f75538a6a29c4c42877da4
ye-s-rin <pyr53540@gmail.com>
2024-05-22 15:47:11 +0900

    refactor: return type ApiUtils.ApiResult

--- src/main/java/com/example/shoppingmall/product/ProductController.java
+++ src/main/java/com/example/shoppingmall/product/ProductController.java
@@ -1,6 +1,11 @@
 package com.example.shoppingmall.product;
 
+import static com.example.shoppingmall.utils.ApiUtils.error;
+import static com.example.shoppingmall.utils.ApiUtils.success;
+
+import com.example.shoppingmall.utils.ApiUtils;
 import com.example.shoppingmall.utils.Validator;
+import jakarta.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.Map;
 import lombok.AllArgsConstructor;
@@ -23,20 +28,20 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping(value = "/products/{id}")
-    public ResponseEntity<Product> findProduct(@PathVariable("id") int id) {
+    public ApiUtils.ApiResult findProduct(@PathVariable("id") int id) {
         if (Validator.isNumber(id)) {
             Product resultProduct = productService.findProduct(id);
             if (resultProduct != null) {
-                return new ResponseEntity<>(resultProduct, HttpStatus.OK);
+                return success(resultProduct);
             } else {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                return error("찾지 못했습니다.", HttpStatus.NOT_FOUND);
             }
         }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return error("잘못된 사용자 요청입니다.", HttpStatus.BAD_REQUEST);
     }
 
     @GetMapping(value = "/products")
-    public ResponseEntity<Map> findProducts(
+    public ApiUtils.ApiResult findProducts(
         @RequestParam(required = false, value = "currentPage") Integer currentPage,
         @RequestParam(required = false, value = "limit") Integer limit,
         @RequestParam(required = false, value = "categoryId") Integer categoryId) {
@@ -44,14 +49,14 @@ public class ProductController {
         Map products = this.productService.findProducts(currentPage, limit, categoryId);
 
         if (products != null) {
-            return new ResponseEntity<>(products, HttpStatus.OK);
+            return success(products);
         } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return error("찾지 못했습니다.", HttpStatus.NOT_FOUND);
         }
     }
 
     @PostMapping(value = "/products")
-    public ResponseEntity registerProduct(@RequestBody Product product) {
+    public ApiUtils.ApiResult registerProduct(@RequestBody Product product) {
         // * 유효성 검사: name(영어), price(숫자)
         // 1) 조건문
         if (Validator.isAlpha(product.getName()) && Validator.isNumber(product.getPrice())) {
@@ -59,45 +64,43 @@ public class ProductController {
             try {
                 log.info(savedProduct.getName());
             } catch (NullPointerException e) {
-                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+                return error("등록하지 못했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
             }
-            return new ResponseEntity<>(HttpStatus.CREATED);
+            return success(savedProduct); // HttpStatus.CREATED
         } else {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return error("잘못된 사용자 요청입니다.", HttpStatus.BAD_REQUEST);
         }
     }
 
     @DeleteMapping(value = "/products/{id}")
-    public ResponseEntity deleteProduct(@PathVariable("id") int id) {
+    public ApiUtils.ApiResult deleteProduct(@PathVariable("id") int id) {
         if (Validator.isNumber(id)) {
             Product deleteProduct = this.productService.deleteProduct(id);
             if (deleteProduct != null) {
-                return new ResponseEntity<>(HttpStatus.OK);
+                return success(deleteProduct);
             } else {
-                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+                return error("삭제하지 못했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return error("잘못된 사용자 요청입니다.", HttpStatus.BAD_REQUEST);
     }
 
+    @Transactional
     @PostMapping(value = "/products/delete")
-    public ResponseEntity deleteProducts(
+    public ApiUtils.ApiResult deleteProducts(
         @RequestBody Map<String, ArrayList<Integer>> deleteRequest) {
-        Product deleteProduct;
-        ResponseEntity result = new ResponseEntity<>(HttpStatus.OK);
-
         for (int id : deleteRequest.get("productIds")) {
             if (Validator.isNumber(id)) {
-                deleteProduct = this.productService.deleteProduct(id);
+                Product deleteProduct = this.productService.deleteProduct(id);
 
                 if (deleteProduct == null) {
-                    result = new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+                    return error("삭제하지 못했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
                 }
             } else {
-                result = new ResponseEntity(HttpStatus.BAD_REQUEST);
+                return error("잘못된 사용자 요청입니다.", HttpStatus.BAD_REQUEST);
             }
         }
 
-        return result;
+        return success(deleteRequest.get("productIds"));
     }
 }
