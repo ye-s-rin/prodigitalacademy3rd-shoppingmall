8dd43babd5825d65885bb4b2d54c31d125eb97a9
ye-s-rin <pyr53540@gmail.com>
2024-05-22 15:25:38 +0900

    refactor: remove unused library

--- src/main/java/com/example/shoppingmall/user/User.java
+++ src/main/java/com/example/shoppingmall/user/User.java
@@ -1,10 +1,7 @@
 package com.example.shoppingmall.user;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
