1046ae0c44a5b6cb413ffa6bc67093eb062d30a1
ye-s-rin <pyr53540@gmail.com>
2024-05-22 15:11:40 +0900

    refactor: use UserJPARepository

--- src/main/java/com/example/shoppingmall/user/UserService.java
+++ src/main/java/com/example/shoppingmall/user/UserService.java
@@ -2,6 +2,7 @@ package com.example.shoppingmall.user;
 
 import jakarta.transaction.Transactional;
 import java.util.Map;
+import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,20 +11,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 @AllArgsConstructor
 public class UserService {
 
-    private UserRepository userRepository;
+    private UserJPARepository userJPARepository;
 
     public void makeConnection() {
-        this.userRepository.makeConnection();
     }
 
     @Transactional
     public UserDTO join(@RequestBody UserDTO userDto) {
-        this.userRepository.save(userDto.convertToEntity());
+        this.userJPARepository.save(userDto.convertToEntity());
 
-        User joinUser = this.userRepository.findByUserId(userDto.getUserId());
+        Optional<User> joinUser = this.userJPARepository.findByUserId(userDto.getUserId());
 
         if (joinUser != null) {
-            return joinUser.convertToDTO();
+            return joinUser.get().convertToDTO();
         } else {
             return null;
         }
@@ -31,7 +31,7 @@ public class UserService {
 
     @Transactional
     public UserDTO login(Map<String, String> loginInfo) {
-        User loginUser = this.userRepository.findByLoginInfo(loginInfo);
+        User loginUser = findByLoginInfo(loginInfo);
 
         if (loginUser != null) {
             return loginUser.convertToDTO();
@@ -41,12 +41,29 @@ public class UserService {
     }
 
     public boolean isDuplicateUserId(String userId) {
-        User existUser = this.userRepository.findByUserId(userId);
+        Optional<User> existUser = this.userJPARepository.findByUserId(userId);
 
         if (existUser != null) {
             return true;
         } else {
             return false;
         }
+    }
+
+    public User findByLoginInfo(Map<String, String> loginInfo) {
+        String userId = loginInfo.get("user_id");
+        String pw = loginInfo.get("pw");
+        Optional<User> user = userJPARepository.findByUserIdAndPw(userId, pw);
+        return user.orElse(null);
+    }
+
+    public User findByUserId(String userId) {
+        Optional<User> user = userJPARepository.findByUserId(userId);
+        return user.orElse(null);
+    }
+
+    public User findById(int id) {
+        Optional<User> user = userJPARepository.findById(id);
+        return user.orElse(null);
     }
 }
