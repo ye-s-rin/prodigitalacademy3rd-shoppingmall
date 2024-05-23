157661acc64c7061f61780f3ab50b48a82432a17
ye-s-rin <pyr53540@gmail.com>
2024-05-23 09:19:17 +0900

    docs: Product SQL

--- shoppingmall.sql
+++ shoppingmall.sql
@@ -1,8 +1,8 @@
 -- 쇼핑몰 데이터베이스 생성
-CREATE DATABASE IF NOT EXISTS shoppingmall;
+CREATE DATABASE IF NOT EXISTS shoppingmalldb;
 
 -- 생성한 데이터베이스 사용
-USE shoppingmall;
+USE shoppingmalldb;
 
 -- 사용자 테이블 생성
 CREATE TABLE IF NOT EXISTS User (
@@ -15,3 +15,16 @@ CREATE TABLE IF NOT EXISTS User (
 );
 
 SELECT * FROM User;
+
+-- 사용자 테이블 생성
+CREATE TABLE IF NOT EXISTS Product (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT NOT NULL UNIQUE,
+    name VARCHAR(10) NOT NULL,
+    price INT NOT NULL,
+    summary VARCHAR(100) NOT NULL UNIQUE,
+    description VARCHAR(500) NOT NULL,
+    category_id INT NOT NULL
+);
+
+SELECT * FROM Product;
