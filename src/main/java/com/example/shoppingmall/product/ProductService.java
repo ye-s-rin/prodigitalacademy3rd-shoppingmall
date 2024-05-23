f04c43b4216ed57d78a418b4e4515764602abfca
ye-s-rin <pyr53540@gmail.com>
2024-05-23 10:56:18 +0900

    refactor: findProducts

--- src/main/java/com/example/shoppingmall/product/ProductService.java
+++ src/main/java/com/example/shoppingmall/product/ProductService.java
@@ -1,5 +1,6 @@
 package com.example.shoppingmall.product;
 
+import java.util.List;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -21,9 +22,46 @@ public class ProductService {
         return product.isEmpty() ? null : product.get().convertToDTO();
     }
 
-    public Map<String, Object> findProducts(int currentPage, int limit) {
+    public Map<String, Object> findProducts(Integer currentPage, Integer limit,
+        Integer categoryId) {
+
+        if (currentPage != null && currentPage < 1) {
+            currentPage = 1;
+        }
+
+        if (limit != null && limit < 1) {
+            limit = 1;
+        }
+
+        if (currentPage == null && limit == null && categoryId == null) {
+            return findAllProducts();
+        } else if (categoryId == null) {
+            return findProductsByPaging(currentPage, limit);
+        } else if (categoryId != null) {
+            return findProductsByCategoryId(currentPage, limit, categoryId);
+        }
+        return null;
+    }
+
+    public Map<String, Object> findAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("products", products);
+
+        return result;
+    }
+
+    public Map<String, Object> findProductsByPaging(int currentPage, int limit) {
         Pageable pageable = PageRequest.of(currentPage - 1, limit);
         Page<Product> productPage = productRepository.findAll(pageable);
+        int lastPage = Math.max(1, productPage.getTotalPages());
+
+        if (currentPage > lastPage) {
+            currentPage = lastPage;
+            pageable = PageRequest.of(currentPage - 1, limit);
+            productPage = productRepository.findAll(pageable);
+        }
 
         Map<String, Object> result = new HashMap<>();
         result.put("products", productPage.getContent());
@@ -34,9 +72,17 @@ public class ProductService {
         return result;
     }
 
-    public Map<String, Object> findProducts(int currentPage, int limit, int categoryId) {
+    public Map<String, Object> findProductsByCategoryId(int currentPage, int limit,
+        int categoryId) {
         Pageable pageable = PageRequest.of(currentPage - 1, limit);
         Page<Product> productPage = productRepository.findByCategoryId(categoryId, pageable);
+        int lastPage = Math.max(1, productPage.getTotalPages());
+
+        if (currentPage > lastPage) {
+            currentPage = lastPage;
+            pageable = PageRequest.of(currentPage - 1, limit);
+            productPage = productRepository.findAll(pageable);
+        }
 
         Map<String, Object> result = new HashMap<>();
         result.put("products", productPage.getContent());
