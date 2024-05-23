b394c8634a54f2c827d1716cf684a25a09758f82
ye-s-rin <pyr53540@gmail.com>
2024-05-23 09:42:48 +0900

    refactor: deleteProduct

--- src/main/java/com/example/shoppingmall/product/ProductRepository.java
+++ src/main/java/com/example/shoppingmall/product/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.example.shoppingmall.product;
 
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,9 @@ import org.springframework.stereotype.Repository;
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     Page<Product> findByCategoryId(int categoryId, Pageable pageable);
 
+    Optional<Product> findByProductId(int productId);
+
     Page<Product> findAll(Pageable pageable);
+
+    void deleteByProductId(int productId);
 }
