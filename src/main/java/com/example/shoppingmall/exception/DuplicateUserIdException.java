5e553dec4116f9f4558b376fc18d6aef88b0f585
ye-s-rin <pyr53540@gmail.com>
2024-05-27 14:03:36 +0900

    refactor

--- src/main/java/com/example/shoppingmall/exception/DuplicateUserIdException.java
+++ src/main/java/com/example/shoppingmall/exception/DuplicateUserIdException.java
@@ -1,8 +1,12 @@
-package com.example.shoppingmall.user;
+package com.example.shoppingmall.exception;
 
 public class DuplicateUserIdException extends RuntimeException {
 
     private String message;
+
+    public DuplicateUserIdException(String message) {
+        this.message = message;
+    }
 
     @Override
     public String getMessage() {
